fd60b64129b5b683d93a2a51cd62bf3635a549e7
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2026-03-10 22:44:15 +0900

    add: Rails_setup

---
+++ backend/app/controllers/api/weather_controller.rb
@@ -0,0 +1,9 @@
+module Api
+  class WeatherController < ApplicationController
+    def create
+      # TODO: Implement WeatherClassifier
+      # Receives audio features from frontend, returns weather result
+      render json: { weather: "sunny", message: "WeatherClassifier not yet implemented" }
+    end
+  end
+end
