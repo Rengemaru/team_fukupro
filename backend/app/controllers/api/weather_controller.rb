e97358580af8f618118f7f6a94952c537a27e707
rengemaru <t5424035@fukuyama-u.ac.jp>
2026-03-17 12:12:23 +0900

    test: 説明用

--- backend/app/controllers/api/weather_controller.rb
+++ backend/app/controllers/api/weather_controller.rb
@@ -3,7 +3,7 @@ module Api
     def create
       # TODO: Implement WeatherClassifier
       # Receives audio features from frontend, returns weather result
-      render json: { weather: "sunny", message: "WeatherClassifier not yet implemented" }
+      render json: {weather: "sunny", message: "WeatherClassifier not yet implemented" }
     end
   end
 end
