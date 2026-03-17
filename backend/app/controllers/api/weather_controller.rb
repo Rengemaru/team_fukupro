3f6eeb672580c1fe77ae2d9a667f51fb5ba2eaeb
rengemaru <t5424035@fukuyama-u.ac.jp>
2026-03-18 02:53:54 +0900

    feat: api/weatherのエンドポイント作成

--- backend/app/controllers/api/weather_controller.rb
+++ backend/app/controllers/api/weather_controller.rb
@@ -1,9 +1,12 @@
 module Api
   class WeatherController < ApplicationController
     def create
-      # TODO: Implement WeatherClassifier
-      # Receives audio features from frontend, returns weather result
-      render json: { weather: "sunny", message: "WeatherClassifier not yet implemented" }
+      frames = params.require(:frames).map do |f|
+        f.permit(:rms, :zcr, :spectral_centroid, :spectral_rolloff).to_h.symbolize_keys
+      end
+
+      weather = WeatherClassifier.new(frames).classify
+      render json: { weather: }
     end
   end
 end
