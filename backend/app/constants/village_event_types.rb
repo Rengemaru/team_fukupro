9b4c3251971d44a4bb07043fc7f4de9e8d8bdac8
rengemaru <t5424035@fukuyama-u.ac.jp>
2026-03-18 14:22:25 +0900

    feat: 村イベント管理の定数ファイルを追加

---
+++ backend/app/constants/village_event_types.rb
@@ -0,0 +1,38 @@
+VILLAGE_EVENT_TYPES = {
+  'drought' => {
+    label:            '村の干ばつ',
+    correct_weathers: [ 'rain', 'hail' ],
+    penalty_weathers: [ 'sunny' ],
+    hp_reward:        1,
+    hp_penalty:       1,
+    success_message:  '恵みの雨が大地を潤した！',
+    penalty_message:  '強い日差しが大地を乾かし、干ばつが悪化した...',
+  },
+  'heavy_rain' => {
+    label:            '大雨',
+    correct_weathers: [ 'sunny' ],
+    penalty_weathers: [ 'rain', 'thunderstorm' ],
+    hp_reward:        1,
+    hp_penalty:       1,
+    success_message:  '晴れ間が大雨を和らげた！',
+    penalty_message:  'さらなる雨が大雨の被害を広げた...',
+  },
+  'sailing' => {
+    label:            '帆船が動かない',
+    correct_weathers: [ 'wind' ],
+    penalty_weathers: [ 'thunderstorm' ],
+    hp_reward:        1,
+    hp_penalty:       1,
+    success_message:  '強風が帆船を力強く動かした！',
+    penalty_message:  '嵐が帆船を危険にさらした...',
+  },
+  'beast' => {
+    label:            '獣の群れ',
+    correct_weathers: [ 'thunderstorm' ],
+    penalty_weathers: [ 'sunny' ],
+    hp_reward:        1,
+    hp_penalty:       1,
+    success_message:  '雷鳴が獣の群れを追い払った！',
+    penalty_message:  '穏やかな天気に獣が活発になった...',
+  },
+}.freeze
--- backend/config/initializers/constants.rb
+++ backend/config/initializers/constants.rb
@@ -1,2 +1,3 @@
 require Rails.root.join("app/constants/map_node")
 require Rails.root.join("app/constants/weather")
+require Rails.root.join("app/constants/village_event_types")
